258aa7fa7492f1b9a51cdd56ee5b7e617207d6c9
ranjith-24-prog <24mranjith@gmail.com>
2025-08-03 21:00:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ def generate_requirements_from_llm(prompt, client):
 
     try:
         response = client.chat.completions.create(
-            model="llama-3.1-sonar-large-128k-online",
+            model="sonar-pro",
             messages=messages
         )
 
